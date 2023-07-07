3af5d105a24a3d41035f574c5b5d6495691bfc27
Ozzie <85667817+xoxosos@users.noreply.github.com>
2023-07-07 16:09:55 +0800

    feat: card

---
+++ src/components/CardItem.tsx
@@ -0,0 +1,60 @@
+/*
+ * @Author: LinRenJie
+ * @Date: 2023-07-04 14:21:01
+ * @LastEditTime: 2023-07-07 15:26:04
+ * @Description: 
+ * @FilePath: \react-test\src\components\CardItem.tsx
+ * @Email: xoxosos666@gmail.com
+ */
+import { Avatar, Col, FlexboxGrid, Panel, Tag } from 'rsuite';
+
+const Card = () => (
+    <Panel className='card-item'  >
+        <div className='card-item-header'>
+            <Avatar
+                size="lg"
+                circle
+                src="https://avatars.githubusercontent.com/u/12592949"
+                alt="@SevenOutman"
+            />
+            <div style={{ overflow: 'hidden', padding: '0 5px 0 3px', flex: '1 1 auto' }}>
+                <h4>title</h4>
+                <p className='card-item-header-text'>contentcontentcontentcontentcontent</p>
+            </div>
+        </div>
+        <div className='card-item-tags'>
+            <Tag size='sm' color="red">Red</Tag>
+            <Tag size='sm' color="orange">Orange</Tag>
+            <Tag size='sm' color="yellow">Yellow</Tag>
+            <Tag size='sm' color="green">Green</Tag>
+            <Tag size='sm' color="cyan">Cyan</Tag>
+            <Tag size='sm' color="blue">Blue</Tag>
+            <Tag size='sm' color="violet">Violet</Tag>
+        </div>
+    </Panel>
+);
+
+export const CardItem = () => (
+    <div className="show-grid">
+        <FlexboxGrid justify="start">
+            <FlexboxGrid.Item as={Col} colspan={24} lg={4} md={8} sm={12}>
+                <Card />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item as={Col} colspan={24} lg={4} md={8} sm={12} >
+                <Card />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item as={Col} colspan={24} lg={4} md={8} sm={12}>
+                <Card />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item as={Col} colspan={24} lg={4} md={8} sm={12}>
+                <Card />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item as={Col} colspan={24} lg={4} md={8} sm={12}>
+                <Card />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item as={Col} colspan={24} lg={4} md={8} sm={12}>
+                <Card />
+            </FlexboxGrid.Item>
+        </FlexboxGrid>
+    </div>
+);
