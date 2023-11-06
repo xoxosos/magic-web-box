7e904368c042c871bb913a1216ba1899aa050d4d
LinRenJie <linrenjie@houseitech.com>
2023-11-06 17:51:53 +0800

    ✨ 优化

--- magic-box-fontend/src/pages/homepage/components/content-header/ImageWrapper.tsx
+++ magic-box-fontend/src/pages/homepage/components/content-header/ImageWrapper.tsx
@@ -1,17 +1,9 @@
-/*
- * @Author: LinRenJie
- * @Date: 2023-07-06 15:44:41
- * @LastEditTime: 2023-09-26 16:52:21
- * @Description:
- * @FilePath: \magic-box-fontend\src\pages\homepage\components\content-header\ImageWrapper.tsx
- * @Email: xoxosos666@gmail.com
- */
 import React from 'react'
 import { ParticleAnimation } from '../../../../components/canvas/canvas'
 import SearchBox from './components/SearchBox'
 import styles from './styles.module.less'
 
-const Notice = () => <p>test</p>
+const Notice = () => <p>这是滚动通知栏</p>
 
 const ImageWrapper: React.FC = () => {
   return (
